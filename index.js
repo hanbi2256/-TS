a1f8382b4bfc1862868ccf3d17ab839475ff0b8c
김한비 <gksql2256@dsm.hs.kr>
2022-11-18 17:15:40 +0900

    🎮 :: TS 테스트코드

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-let 이름 = 'kim';
+// let 이름: string = "kim";
+// let 나이: number = 50;
+// let 회원들 : string[]=['kim', 'park'];
+let 학교 = {
+    score: [100, 97, 84],
+    teacher: "Phil",
+    friend: "John",
+};
+학교.score[4] = false;
+학교.friend = ["Lee", 학교.teacher];
--- index.ts
+++ index.ts
@@ -1,5 +1,15 @@
+// let 이름: string = "kim";
+// let 나이: number = 50;
+// let 회원들 : string[]=['kim', 'park'];
 
-type 내타입 =string|number;
-
-let 이름:내타입=123;
-
+let 학교: {
+  score: (number | boolean)[];
+  teacher: string;
+  friend: string | string[];
+} = {
+  score: [100, 97, 84],
+  teacher: "Phil",
+  friend: "John",
+};
+학교.score[4] = false;
+학교.friend = ["Lee", 학교.teacher];
